c3d7608a878faafde3a73f38037d5911d468a243
rsharit <altiusfortiuscitius@gmail.com>
2021-09-16 14:51:25 +0530

    Adding updated pom + gitignore
    + Also adding more demo code to verify set up so far.

--- testvagrant/src/test/java/DemoTest/DemoCheck.java
+++ testvagrant/src/test/java/DemoTest/DemoCheck.java
@@ -5,6 +5,7 @@ import org.apache.juneau.serializer.SerializeException;
 import util.deserializer.Deserializer;
 import util.serializer.Serializer;
 import weather.pojo.Clouds;
+import weather.pojo.Main;
 
 public class DemoCheck {
     public static void main(String args[]) throws SerializeException, ParseException {
@@ -18,6 +19,21 @@ public class DemoCheck {
         Deserializer deserializerObj = new Deserializer();
         Clouds cloud2 = deserializerObj.getDeserializedObj(jsonString, Clouds.class);
         System.out.println();
+
+        Main main = new Main();
+        main.setFeels_like(303.3);
+        main.setTemp(299.9);
+        main.setTemp_max(299.9);
+        main.setTemp_min(280.2);
+        main.setGrnd_level(1010);
+        main.setHumidity(93);
+
+        jsonString = serializerObj.getJsonString(main);
+        Main main2 = deserializerObj.getDeserializedObj(jsonString, Main.class);
+        System.out.println();
+
+
+
 
     }
 }
